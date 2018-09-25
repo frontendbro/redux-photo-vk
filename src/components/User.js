b2df5c9b5a374ebb69d4d1c040272efd946f25bf
s.zadorozhnyy <s.zadorozhnyy@1tvch.ru>
2018-09-25 12:51:55 +0300

    Divide by Containers & Components

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,9 @@
-import react from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 export class User extends React.Component {
   render() {
-    const {name} = this.this.props.
+    const {name} = this.props;
     return(
       <div>
         <p>Привет, {name}!</p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {User} from '../components/User';
+import {Page} from '../components/Page';
 import './App.css';
 
 class App extends Component {
@@ -10,10 +12,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p className="App-intro">
-          Привет из App, {user.name}!
-        </p>
-        <p>У тебя {page.photos.length} фото за {page.year}?</p>
+        <User name={user.name} />
+        <Page photos={page.photos} year={page.year} />
       </div>
     )
   }
